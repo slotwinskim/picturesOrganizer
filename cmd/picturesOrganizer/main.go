c1bb17672d31ef06dce647984286ec68982f2203
Marcin Słotwiński (MNSL) <mnsl@demant.com>
2020-05-01 20:26:59 +0200

    Started imlementing tree command

--- cmd/picturesOrganizer/main.go
+++ cmd/picturesOrganizer/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello world")
+
+	root := "./"
+	tree(root)
+
+}
+
+func tree(path string) {
+	files, err := ioutil.ReadDir(path)
+
+	if err != nil {
+		fmt.Println("Error")
+		return
+	}
+
+	for nr, file := range files {
+		if !file.IsDir() {
+			pre := "├─── "
+			if nr == len(files)-1 {
+				pre = "└─── "
+			}
+
+			fullName := pre + getProperName(file.Name())
+			fmt.Println(fullName)
+		} else {
+			fmt.Println(getProperName(file.Name()))
+			tree(path + file.Name())
+		}
+	}
+}
+
+func getProperName(name string) string {
+	if !strings.Contains(name, " ") {
+		return name
+	} else {
+		return "\"" + name + "\""
+	}
 }
